bd2a7e2bea961077ec0d4ed187c44c7884bf9da5
Anushka Samad <anushkasamad276@gmail.com>
2026-02-02 19:34:08 +0500

    Add withdraw method with balance validation logic
    Completed the Bank Account system by adding a secure withdraw method. Implemented logic to check for sufficient funds before allowing a transaction

--- encapsulation_setter.py
+++ encapsulation_setter.py
@@ -1,7 +1,7 @@
 class BankAccount:
     def __init__(self, name, money):
         self.name = name
-        self.__balance = money # Locked Variable 🔒
+        self.__balance = money # Locked Variable 
 
     # 1. Getter (Dekhne ke liye - Kal kiya tha)
     def get_balance(self):
@@ -17,5 +17,38 @@ class BankAccount:
 
 # Bas chota sa test
 my_acc = BankAccount("Anushka", 5000)
-my_acc.deposit(2000) # Ab balance 7000 ho jayega ✅
+my_acc.deposit(2000) # Ab balance 7000 ho jayega 
 print("Total:", my_acc.get_balance())
+
+
+
+
+# for withdrow
+class BankAccount:
+    def __init__(self, name, money):
+        self.name = name
+        self.__balance = money
+
+    def get_balance(self):
+        return self.__balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Deposited: {amount}")
+        else:
+            print("Error: Negative value!")
+
+    # NAYA KAAM: Withdraw Method (Paise nikalna)
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance: # Check: Paise hain bhi ya nahi?
+            self.__balance -= amount
+            print(f"Success! Withdrew {amount} rupees.")
+        else:
+            print("Error: Insufficient balance or invalid amount!")
+
+# Testing
+acc = BankAccount("Anushka", 7000)
+acc.withdraw(3000) # Chalna chahiye (Balance 4000 bachega) 
+acc.withdraw(10000) # Error aana chahiye (Paise kam hain) 
+print("Final Balance:", acc.get_balance())
